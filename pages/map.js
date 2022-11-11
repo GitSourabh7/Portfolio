6553ad6c62476348731a4e9caf5518ac1946d630
gitsourabh7 <sourabhnerlekar22@gmail.com>
2022-11-11 13:16:17 +0530

    Update map.js
    Replace Jayesh with Sourabh, location change, iframe height update

--- pages/map.js
+++ pages/map.js
@@ -9,14 +9,14 @@ const Map = () => {
   return (
     <div>
       <TitleDesc
-        title="Jayesh's Location"
-        desc="Here mentioned location of Jayesh."
+        title="Sourabh's Location"
+        desc="Here mentioned location of Sourabh."
       />
       <MapContainer>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d929.32728792598!2d76.22046682915997!3d21.298797248621796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x37ae240af9e11fd!2zMjHCsDE3JzU1LjciTiA3NsKwMTMnMTUuNyJF!5e0!3m2!1sen!2sin!4v1658329089863!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3867250.9286021274!2d76.77476745!3d18.815426549999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcfc41e9c9cd6f9%3A0x1b2f22924be04fb6!2sMaharashtra!5e0!3m2!1sen!2sin!4v1668151509320!5m2!1sen!2sin"
           width="100%"
-          height="450"
+          height="550"
           style={{ border: "0px", ...(darkMode && { filter: "invert(90%)" }) }}
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
