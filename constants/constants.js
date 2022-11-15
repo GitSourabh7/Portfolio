92604b122eb1cc2c5fef2024a2c211a0a7fcdf39
gitsourabh7 <sourabhnerlekar22@gmail.com>
2022-11-15 14:06:22 +0530

    Add projectData in constants.js
    Add project images

--- constants/constants.js
+++ constants/constants.js
@@ -56,7 +56,7 @@ export const info = {
     {
       title: "Google Portfolio (Nov 2022 - present) :",
       description:
-        "Technologies used: Next.js, Styled-components, Javascript, Lottie Animations, SVGs, Babel, ESlint, HTML, CSS",
+        "Technologies used: Next.js, Styled-components, Javascript, Lottie Animations, SVG, Babel, ESlint, HTML, CSS",
     },
     {
       title: "FreeCodeCamp UI Clone (Oct 2022) :",
@@ -120,28 +120,28 @@ export const accordionData = [
 
 export const projectVideos = [
   {
-    heading: "Mini Laptop Implementation using React Js.",
-    platform: `Linkedin.`,
+    heading: "Google Porfolio",
+    platform: `YouTube`,
     author: `Sourabh Nerlekar`,
-    image: `/assets/laptopdesign.png`,
-    date: `1-Mar-2022`,
-    link: `https://www.linkedin.com/feed/update/urn:li:ugcPost:6904281486057373696/`,
+    image: `/assets/GooglePortfolio.png`,
+    date: `15-Nov-2022`,
+    link: ``,
   },
   {
-    heading: "House Design with pure CSS [2022].",
-    platform: `Linkedin.`,
+    heading: "RoboFriends",
+    platform: `YouTube`,
     author: `Sourabh Nerlekar`,
-    image: `/assets/housedesign.png`,
-    date: `10-Jan-2022`,
-    link: `https://www.linkedin.com/posts/jayesh-choudhary-72444518b_css-animation-perspective3d-activity-6886298154199773184-C74d`,
+    image: `/assets/RoboFriends.png`,
+    date: `30-Aug-2022`,
+    link: ``,
   },
   {
-    heading: "IPL-2022 Web Application using React Js.",
-    platform: `Linkedin.`,
+    heading: "UI_Navbar",
+    platform: `YouTube`,
     author: `Sourabh Nerlekar`,
-    image: `/assets/ipl2022.png`,
-    date: `26-Mar-2022`,
-    link: `https://www.linkedin.com/feed/update/urn:li:activity:6913334580070498304/`,
+    image: `/assets/UI_Navbar.png`,
+    date: `16-Apr-2022`,
+    link: ``,
   },
 ];
 
@@ -384,112 +384,137 @@ export const otherSkillsData = {
 
 export const projectsData = [
   {
-    title: "Laptop with React Js and Tailwind CSS",
-    image: "/assets/laptopdesign.png",
-    description: [
-      "Implemented a laptop with Power on, Sign In with password or finger scanner, Restart, Sleep and Shut down functionality using React and Tailwind CSS.",
-    ],
-    videoDemo:
-      "https://www.linkedin.com/feed/update/urn:li:ugcPost:6904281486057373696/",
-    liveDemo: "https://jayesh2906.github.io/laptop-design/",
-    sourceCode: "https://github.com/jayesh2906/laptop-design",
+    title: "Google Portfolio using NextJS",
+    image: "/assets/GooglePortfolio.png",
+    description: ["----"],
+    videoDemo: "",
+    liveDemo: "https://yo-sourabh.netlify.app/",
+    sourceCode: "https://github.com/GitSourabh7/Portfolio",
   },
   {
-    title: "House Design with CSS",
-    image: "/assets/housedesign.png",
-    description: [
-      "Designed front-view of my house with pure CSS 3, Html 5 and JavaScript(for handling events).",
-      "User can open and close Gate, Windows and Slide of a House, User can also rotate a house in 360°.",
-    ],
-    videoDemo:
-      "https://www.linkedin.com/posts/jayesh-choudhary-72444518b_css-animation-perspective3d-activity-6886298154199773184-C74d",
-    liveDemo: "https://jayesh2906.github.io/My-house.github.io/house.html",
-    sourceCode: "https://github.com/jayesh2906/My-house.github.io",
-  },
-  {
-    title: "IPL-2022 Web Application",
-    image: "/assets/ipl2022.png",
-    description: [
-      "Implemented a WebApp that provides the live scorecard of IPL-2022 fetched from RESTful cricket API.",
-      "User can create his/her playing 11 team for each franchise, User can also watch the best of IPL highlights.",
-      "Converted to PWA that can be installed and run on mobile as a native app.",
-      "Deployed it to Netlify.",
-      "Technologies used: JavaScript, React, CSS 3, Material Ui, Local Storage.",
-    ],
-    videoDemo:
-      "https://www.linkedin.com/feed/update/urn:li:activity:6913334580070498304/",
-    liveDemo: "https://ipl-2022.netlify.app/",
-    sourceCode: "https://github.com/jayesh2906/IPL-2022-App",
-  },
-  {
-    title: "Full E-Commerce Web Application",
-    image: "/assets/iconstore.png",
-    description: [
-      "Implemented a Full E-Commerce WebApp with MERN Stack that can be used by users, sellers, and Admin.",
-      "Uses json web token for security, Authorization.",
-      "Uses Geo-location for auto-filling shipping address of user.",
-      "Uses Twilio message notification to registered mobile number on confirmation of orders.",
-      "Uses Paypal payment gateway integration for demo virtual payment of orders.",
-      "Technologies used: JavaScript, React, Redux Thunk, Node Js, Expess Js, Html, CSS, Bootstrap 5, MongoDB.",
-    ],
-    videoDemo:
-      "https://www.linkedin.com/feed/update/urn:li:activity:6842941644195672064/",
+    title: "RoboFriends using ReactJS",
+    image: "/assets/RoboFriends.png",
+    description: ["----"],
+    videoDemo: "/assets/RoboFriends.mp4",
     liveDemo: "",
-    sourceCode: "https://github.com/jayesh2906/Final-Ecommerce-Project",
+    sourceCode: "https://github.com/GitSourabh7/robofriends",
   },
   {
-    title: "Ice Mart Mobile Application",
-    image: "/assets/icemart.png",
-    description: [
-      "Implemented an Ice Mart Mobile App with MERNN Stack that can be used by users, and Admin.",
-      "Uses json web token for security, Authorization.",
-      "Uses Google-maps for location of user.",
-      "Uses Google pay payment gateway integration for demo virtual payment of orders.",
-      "Technologies used: JavaScript, React Native, Redux Toolkit, Node Js, Expess Js, MongoDB.",
-    ],
-    videoDemo:
-      "https://www.linkedin.com/feed/update/urn:li:activity:6941967333682737152/",
+    title: "Responsive Sidebar",
+    image: "/assets/UI_Navbar.png",
+    description: ["----", "----"],
+    videoDemo: "/assets/UI_Navbar.mp4",
     liveDemo: "",
-    sourceCode: "https://github.com/jayesh2906/Ice-Mart",
+    sourceCode: "https://github.com/GitSourabh7/UI_Navbar",
   },
   {
-    title: "Real-time Online Chat Application",
-    image: "/assets/chatapp.png",
-    description: [
-      "Implemented a Real-time Online Chat Application with react and stream that can be used for online communication.",
-      "Server deployed to Heroku and Client deployed to Netlify.",
-      "Responsive and converted into PWA(Native app).",
-      "Technologies used: JavaScript, React, Stream, Node Js, Expess Js, Html, CSS.",
-    ],
-    videoDemo:
-      "https://www.linkedin.com/feed/update/urn:li:activity:6877141876273762304/",
+    title: "FreeCodeCamp UI Clone ",
+    image: "/assets/FreeCodeCamp.png",
+    description: ["----", "----"],
+    videoDemo: "/assets/FreeCodeCamp.mp4",
     liveDemo: "",
-    sourceCode: "https://github.com/jayesh2906/Lets-Chat-app",
+    sourceCode: "https://github.com/GitSourabh7/FreeCodeCamp",
   },
   {
-    title: "Movie Info Application",
-    image: "/assets/movieapp.png",
-    description: [
-      "Implemented a Movie info app with MERN Stack with third-party APIs for fetching movies record, Uses OMDb RESTful web service.",
-      "Technologies used: JavaScript, React, Node Js, Expess Js, Html, CSS, Bootstrap 5, MongoDB.",
-    ],
-    videoDemo:
-      "https://www.linkedin.com/feed/update/urn:li:activity:6842939354764201984/",
+    title: "Moshify: The Cloud-Hosting Site ",
+    image: "/assets/Moshify.png",
+    description: ["----", "----"],
+    videoDemo: "/assets/Moshify.mp4",
     liveDemo: "",
-    sourceCode: "",
+    sourceCode: "https://github.com/GitSourabh7/FreeCodeCamp",
   },
   {
-    title: "All Projects Compilation",
-    image: "/assets/allProjects.png",
-    description: [
-      "Check here my all Projects in Video Compilation.",
-      "Technologies used: JavaScript, React Js, Redux, React Native, Next Js, Html5, CSS3,Tailwind Css, Material Ui, Bootstrap5, Sass, Styled Component, Node Js, Express Js, Mongo Db, Postman and Jwt-Authentication.",
-    ],
-    videoDemo:
-      "https://www.linkedin.com/feed/update/urn:li:activity:6962305778502254592/",
+    title: "Vidly: The Movie Renting Service",
+    image: "/assets/Vidly.png",
+    description: ["----", "----"],
+    videoDemo: "/assets/Vidly.mp4",
     liveDemo: "",
-    sourceCode: "",
+    sourceCode: "https://github.com/GitSourabh7/vidly",
   },
+  // {
+  //   title: "IPL-2022 Web Application",
+  //   image: "/assets/ipl2022.png",
+  //   description: [
+  //     "Implemented a WebApp that provides the live scorecard of IPL-2022 fetched from RESTful cricket API.",
+  //     "User can create his/her playing 11 team for each franchise, User can also watch the best of IPL highlights.",
+  //     "Converted to PWA that can be installed and run on mobile as a native app.",
+  //     "Deployed it to Netlify.",
+  //     "Technologies used: JavaScript, React, CSS 3, Material Ui, Local Storage.",
+  //   ],
+  //   videoDemo:
+  //     "https://www.linkedin.com/feed/update/urn:li:activity:6913334580070498304/",
+  //   liveDemo: "https://ipl-2022.netlify.app/",
+  //   sourceCode: "https://github.com/jayesh2906/IPL-2022-App",
+  // },
+  // {
+  //   title: "Full E-Commerce Web Application",
+  //   image: "/assets/iconstore.png",
+  //   description: [
+  //     "Implemented a Full E-Commerce WebApp with MERN Stack that can be used by users, sellers, and Admin.",
+  //     "Uses json web token for security, Authorization.",
+  //     "Uses Geo-location for auto-filling shipping address of user.",
+  //     "Uses Twilio message notification to registered mobile number on confirmation of orders.",
+  //     "Uses Paypal payment gateway integration for demo virtual payment of orders.",
+  //     "Technologies used: JavaScript, React, Redux Thunk, Node Js, Expess Js, Html, CSS, Bootstrap 5, MongoDB.",
+  //   ],
+  //   videoDemo:
+  //     "https://www.linkedin.com/feed/update/urn:li:activity:6842941644195672064/",
+  //   liveDemo: "",
+  //   sourceCode: "https://github.com/jayesh2906/Final-Ecommerce-Project",
+  // },
+  // {
+  //   title: "Ice Mart Mobile Application",
+  //   image: "/assets/icemart.png",
+  //   description: [
+  //     "Implemented an Ice Mart Mobile App with MERNN Stack that can be used by users, and Admin.",
+  //     "Uses json web token for security, Authorization.",
+  //     "Uses Google-maps for location of user.",
+  //     "Uses Google pay payment gateway integration for demo virtual payment of orders.",
+  //     "Technologies used: JavaScript, React Native, Redux Toolkit, Node Js, Expess Js, MongoDB.",
+  //   ],
+  //   videoDemo:
+  //     "https://www.linkedin.com/feed/update/urn:li:activity:6941967333682737152/",
+  //   liveDemo: "",
+  //   sourceCode: "https://github.com/jayesh2906/Ice-Mart",
+  // },
+  // {
+  //   title: "Real-time Online Chat Application",
+  //   image: "/assets/chatapp.png",
+  //   description: [
+  //     "Implemented a Real-time Online Chat Application with react and stream that can be used for online communication.",
+  //     "Server deployed to Heroku and Client deployed to Netlify.",
+  //     "Responsive and converted into PWA(Native app).",
+  //     "Technologies used: JavaScript, React, Stream, Node Js, Expess Js, Html, CSS.",
+  //   ],
+  //   videoDemo:
+  //     "https://www.linkedin.com/feed/update/urn:li:activity:6877141876273762304/",
+  //   liveDemo: "",
+  //   sourceCode: "https://github.com/jayesh2906/Lets-Chat-app",
+  // },
+  // {
+  //   title: "Movie Info Application",
+  //   image: "/assets/movieapp.png",
+  //   description: [
+  //     "Implemented a Movie info app with MERN Stack with third-party APIs for fetching movies record, Uses OMDb RESTful web service.",
+  //     "Technologies used: JavaScript, React, Node Js, Expess Js, Html, CSS, Bootstrap 5, MongoDB.",
+  //   ],
+  //   videoDemo:
+  //     "https://www.linkedin.com/feed/update/urn:li:activity:6842939354764201984/",
+  //   liveDemo: "",
+  //   sourceCode: "",
+  // },
+  // {
+  //   title: "All Projects Compilation",
+  //   image: "/assets/allProjects.png",
+  //   description: [
+  //     "Check here my all Projects in Video Compilation.",
+  //     "Technologies used: JavaScript, React Js, Redux, React Native, Next Js, Html5, CSS3,Tailwind Css, Material Ui, Bootstrap5, Sass, Styled Component, Node Js, Express Js, Mongo Db, Postman and Jwt-Authentication.",
+  //   ],
+  //   videoDemo:
+  //     "https://www.linkedin.com/feed/update/urn:li:activity:6962305778502254592/",
+  //   liveDemo: "",
+  //   sourceCode: "",
+  // },
 ];
 
 export const contactDetails = [
