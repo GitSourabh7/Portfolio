c741913a99dae48c6362cf3de4b35d710d84ec7f
gitsourabh7 <sourabhnerlekar22@gmail.com>
2022-11-15 14:43:26 +0530

    Update Map.styled.js
    Fix loader not working

--- styles/Map.styled.js
+++ styles/Map.styled.js
@@ -3,8 +3,7 @@ import styled from "styled-components";
 export const MapContainer = styled.main`
   margin-top: 2rem;
   padding: 0 calc(2rem);
-  background: url(https://lottiefiles.com/85487-location) center center
-    no-repeat;
+  background: url(/assets/loader.gif) center center no-repeat;
   background-size: 7%;
   @media ${(props) => props.theme.breakpoints.lg} {
     padding: 0 calc(2rem);
